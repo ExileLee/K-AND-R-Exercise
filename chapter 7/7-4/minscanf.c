3978c640bacf271ed9c54bfa9a3c8963d77d34d4
ExileLee <exile1030@gmail.com>
2020-08-03 21:34:58 +0800

    Modify 7-4: add float type

--- chapter 7/7-4/minscanf.c
+++ chapter 7/7-4/minscanf.c
@@ -14,10 +14,11 @@ void ungetch(int);
 int main(int argc, char *argv[])
 {
     int day, year;
+    float f;
     char month[10];
-    int result = minscanf("%d %s %d", &day, month, &year);
+    int result = minscanf("%d %s %d %f", &day, month, &year, &f);
 
-    printf("result(%d): %d %s %d\n", result, day, month, year);
+    printf("result(%d): %d %s %d %.1f\n", result, day, month, year, f);
 
     return 0;
 }
@@ -25,6 +26,7 @@ int main(int argc, char *argv[])
 int minscanf(char *fmt, ...)
 {
     int *dval, c, fieldLen, argCount;
+    float *fval;
     argCount = 0;
     char *sval, line[MAXLEN];
     va_list ap;
@@ -61,6 +63,20 @@ int minscanf(char *fmt, ...)
                 ungetch(c);
                 *sval = '\0';
                 break;
+            case 'f':
+                while (isnumber((c = getch())))
+                    line[fieldLen++] = c;
+                if (c == '.')
+                {
+                    line[fieldLen++] = c;
+                    while (isnumber((c = getch())))
+                        line[fieldLen++] = c;
+                    ungetch(c);
+                    line[fieldLen] = '\0';
+                    fval = va_arg(ap, float *);
+                    *fval = strtof(line, NULL);    
+                }
+                break;
             default:
                 break;
             }
@@ -71,7 +87,7 @@ int minscanf(char *fmt, ...)
         if (fieldLen > 0)
             argCount++;
     }
-    
+
     va_end(ap);
 
     return argCount;
